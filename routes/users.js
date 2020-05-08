fa75990ad8873106d7ef4652501d47d437cc34ef
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-08 12:22:53 -0500

    Added in validation for user route 30min

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const { check, validationResult } = require("express-validator/check");
+const User = require("../models/User");
 //@route    POST api/users
 //@desc     Register a user
 //@access   Public
-router.post('/', (req, res) => {
-    res.send('Register a user')
-});
+router.post(
+  "/",
+  [
+    check("name", "please enter a name").not().isEmpty(),
+    check("email", "Please enter an email address").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 4 or more characters"
+    ).isLength({ min: 4 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("success!");
+  }
+);
 
 module.exports = router;
