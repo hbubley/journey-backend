ec8055fdda5a86a3aaee81182052e6b99941932f
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-08 14:21:57 -0500

    JWT token created 40min

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require('config');
 const User = require("../models/User");
 //@route    POST api/users
 //@desc     Register a user
@@ -37,16 +39,27 @@ router.post(
         password,
       });
 
-      const salt = await bcrypt.genSalt(10)
+      const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send('User Saved')
 
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), {
+        expiresIn: 360000
+      }, (err, token) => {
+        if(err) throw err;
+        res.json({ token })
+      })
     } catch (err) {
-        console.error(err.message)
-        res.status(500).send('server error')
+      console.error(err.message);
+      res.status(500).send("server error");
     }
   }
 );
