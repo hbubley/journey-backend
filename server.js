07e185996a6e999c6375fce5bf569c0537c71ef4
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-11 13:17:55 -0500

    Trying to hook up backend, cors !!

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const app = express();
 
 connectDB();
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json({ extended: false }));
 
 app.use("/api/users", require("./routes/users"));
