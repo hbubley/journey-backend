a760d150ed58ebd1b43241acabe9b572bd1df60b
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-11 14:12:33 -0500

    COOOORS

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const app = express();
 app.use(cors())
 
 connectDB();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*') // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   
+  app.get('/', function(req, res, next) {
+    app.use("/api/auth", require("./routes/auth"));
+  });
+  
+  app.post('/', function(req, res, next) {
+    app.use("/api/auth", require("./routes/auth"));
+  });
 app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
